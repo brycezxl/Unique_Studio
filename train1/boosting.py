3449cdc4d0cde8101e7dfbf506c67059738eb82f
brycezxl <vincentlei@163.com>
2018-11-03 18:38:07 +0800

    .gitignore

--- train1/boosting.py
+++ train1/boosting.py
@@ -4,11 +4,11 @@ import pandas as pd
 from tqdm import tqdm
 from sklearn.feature_extraction import DictVectorizer
 
+# Adaboost
 
 # 超参
 EPOCH = 3000
-LAMBDA = 0
-AlPHA = 0.1
+AlPHA = 0.01
 
 
 def load_train():
@@ -29,14 +29,6 @@ def load_train():
     del data_frame['Cabin']
     del data_frame['Name']
     del data_frame['Ticket']
-
-    # 特征向量化
-    dvec = DictVectorizer(sparse=False)
-    data_get = dvec.fit_transform(data_frame.to_dict(orient='record'))
-
-    x_get = data_get[:, :-2]
-    y_get = data_get[:, -1]
-    return x_get, y_get
 
 
 def load_test():
@@ -116,19 +108,17 @@ class LogisticRegression(object):
         self.j = -1 / self.rows * (np.sum((self.labels * np.log(self.predict)), axis=0)
                                    + np.sum(((1 - self.labels) * np.log(1 - self.predict)), axis=0))
         self.j_list.append(self.j)
-        print("cost : ", self.j)
         return 0
 
     def back(self):
         """do backprop to adjust theta"""
         grad = AlPHA / self.rows * np.dot(self.images.T, (self.predict - self.labels))
         self.theta = self.theta - grad
-        print("grad : ", grad)
         return 0
 
     def plot_j(self):
         """Visualize the decline of j"""
-        plt.plot(range(EPOCH), self.j_list, c="r")
+        plt.scatter(range(EPOCH), self.j_list, linewidths=0.01, c="r")
         plt.show()
         return 0
 
@@ -159,89 +149,19 @@ def acc():
     return 0
 
 
-def precision_recall_f1_roc_auc(beta=1):
-    """计算查准率与查全率并图示，打印f-beta度量、宏f1，微f1, ROC, AUC
-
-    Args:
-        beta: if beta < 1, emphasis on precision
-              if beta > 1, emphasis on recall
-    """
-
-    sort_predict = np.sort(logistic_regression_test.predict, axis=0)
-    tp = np.zeros((logistic_regression_test.rows, 1))
-    fp = np.zeros((logistic_regression_test.rows, 1))
-    fn = np.zeros((logistic_regression_test.rows, 1))
-    tn = np.zeros((logistic_regression_test.rows, 1))
-    predict_copy2 = np.zeros((logistic_regression_test.rows, 1))
-    for k in range(logistic_regression_test.rows):
-        for l in range(logistic_regression_test.rows):
-
-            # 处理predict
-            if logistic_regression_test.predict[l] < sort_predict[logistic_regression_test.rows - 1 - k]:
-                predict_copy2[l] = 0
-            else:
-                predict_copy2[l] = 1
-
-            # 计算混淆矩阵
-            if predict_copy2[l] == y_test[l] and y_test[l] == 1:
-                tp[k] = tp[k] + 1
-            elif predict_copy2[l] == y_test[l] and y_test[l] == 0:
-                tn[k] = tn[k] + 1
-            elif predict_copy2[l] != y_test[l] and y_test[l] == 1:
-                fn[k] = fn[k] + 1
-            else:
-                fp[k] = fp[k] + 1
-
-    p = tp / (tp + fp)  # 查准率
-    r = tp / (tp + fn)  # 查全率
-    plt.figure(1)
-    plt.subplot(121)
-    plt.xlabel('P')
-    plt.ylabel('R')
-    plt.title('P-R')
-    plt.plot(p, r)
-
-    # F_beta度量(根据beta比较学习器，重视较小值)
-    f_beta = ((1 + beta ** 2) * r * p) / (beta ** 2 * p + r)
-    f_beta = np.mean(f_beta, axis=0)
-    print("F_beta:         %.2f" % f_beta)
-
-    # 宏F1
-    macro_p = np.sum(p, axis=0) / logistic_regression_test.rows
-    macro_r = np.sum(r, axis=0) / logistic_regression_test.rows
-    macro_f1 = 2 * macro_r * macro_p / (macro_r + macro_p)
-    print("macro-F1:       %.2f" % macro_f1)
-
-    # 微F1
-    micro_tp = np.mean(tp, axis=0)
-    micro_fn = np.mean(fn, axis=0)
-    micro_fp = np.mean(fp, axis=0)
-    micro_p = micro_tp / (micro_tp + micro_fp)
-    micro_r = micro_tp / (micro_tp + micro_fn)
-    micro_f1 = float(2 * micro_p * micro_r / (micro_p + micro_r))
-    print("micro-F1:       %.2f" % micro_f1)
-
-    # ROC
-    tpr = tp / (tp + fn)
-    fpr = fp / (tn + fp)
-    plt.subplot(122)
-    plt.xlabel('FPR')
-    plt.ylabel('TPR')
-    plt.title('ROC')
-    plt.plot(fpr, tpr)
-    plt.show()
-
-    # AUC(ROC的面积）
-    auc = 0
-    for f in range(logistic_regression_test.rows - 1):
-        auc = auc + 1 / 2 * (tpr[f + 1] - tpr[f]) * (fpr[f] + fpr[f + 1])
-    print("AUC:            %.2f" % (1 - auc))
-
-    return 0
-
-
-# train
+# load
 x, y = load_train()
+row, col = np.shape(x)
+
+# 初始化权重
+D = np.array([1 / row] * row)
+
+for i in range(EPOCH):
+    
+
+
+
+
 logistic_regression = LogisticRegression(x, y)
 logistic_regression.normalize()
 
@@ -268,4 +188,3 @@ logistic_regression_test.forward()
 
 # 评估
 acc()
-precision_recall_f1_roc_auc()
---
+++ train1/test.py
@@ -0,0 +1,3 @@
+import numpy as np
+D = np.array([1 / 2] * 2)
+print(D)
